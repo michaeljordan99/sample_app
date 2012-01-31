3bdbd7e0e1a5f38eba01afb3c934f6ffee57c44d
michaeljordan99 <michaeljordan99@hotmail.com>
2012-01-31 10:03:23 -0800

    Done with Sign In

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,6 +28,11 @@ class User < ActiveRecord::Base
     return user if user.has_password?(submitted_password)
   end
 
+  def self.authenticate_with_salt(id, cookie_salt)
+    user = find_by_id(id)
+    (user && user.salt == cookie_salt) ? user : nil
+  end
+
   def self.find_me(email)
     user = find_by_email(email)
     return nil if user.nil?
