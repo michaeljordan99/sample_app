d8001b81a80194c1075bc50159fa853dcaa44606
michaeljordan99 <michaeljordan99@hotmail.com>
2012-01-31 14:09:06 -0800

    List Users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  before_filter :authenticate, :only => [:index, :edit, :update]
+  before_filter :correct_user, :only => [:edit, :update]
+
+  def index
+    @title = "All Users"
+    @users = User.all
+  end
 
   def show
     @user = User.find(params[:id])
@@ -8,6 +15,11 @@ class UsersController < ApplicationController
   def new
     @user = User.new
     @title = "Sign Up"
+  end
+
+  def edit
+    @user = User.find(params[:id])
+    @title = "Edit User"
   end
 
   def create
@@ -20,6 +32,28 @@ class UsersController < ApplicationController
       @title = "Sign Up"
       render 'new'
     end
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(params[:user])
+      flash[:success] = "Profile Updated."
+      redirect_to @user
+    else
+      @title = "Edit User"
+      render 'edit'
+    end
+  end
+
+  private
+
+  def authenticate
+    deny_access unless signed_in?
+  end
+
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(home_path) unless current_user?(@user)
   end
 
 end
